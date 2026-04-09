91329832a98a5958a1661434770d6e9124807424
shakina <shakinaabuthahir@gmail.com>
2026-04-09 12:12:06 +0530

    mem 2 first commit

---
+++ PharmacyAPI/Controllers/OrderController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PharmacyAPI.Data;
+using PharmacyAPI.DTOs;
+using PharmacyAPI.Models;
+
+
+namespace PharmacyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public OrderController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //place order
+        [HttpPost("place")]
+        public async Task<IActionResult> PlaceOrder([FromBody] PlaceOrderDto dto)
+        {
+            var order = new Order
+            {
+                UserId = dto.UserId,
+                OrderDate = DateTime.UtcNow,
+                Status = "Placed"
+            };
+
+            decimal total = 0;
+
+            foreach (var item in dto.Items)
+            {
+                var medicine = await _context.Medicines.FindAsync(item.MedicineId);
+
+                if (medicine == null)
+                    return BadRequest($"Medicine ID {item.MedicineId} not found");
+
+                if (medicine.StockQuantity < item.Quantity)
+                    return BadRequest($"Not enough stock for {medicine.Name}");
+
+                var orderItem = new OrderItem
+                {
+                    MedicineId = item.MedicineId,
+                    Quantity = item.Quantity,
+                    Price = medicine.Price
+                };
+
+                total += medicine.Price * item.Quantity;
+
+                medicine.StockQuantity -= item.Quantity;
+
+                order.OrderItems.Add(orderItem);
+            }
+
+            order.TotalAmount = total;
+
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            return Ok(order);
+        }
+
+        //
+        [HttpGet("user/{userId}")]
+        public async Task<IActionResult> GetOrders(int userId)
+        {
+            var orders = await _context.Orders
+                .Include(o => o.OrderItems)
+                .ThenInclude(oi => oi.Medicine)
+                .Where(o => o.UserId == userId)
+                .ToListAsync();
+
+            return Ok(orders);
+        }
+
+        //add order status[admin]
+
+        [HttpPut("status/{orderId}")]
+        public async Task<IActionResult> UpdateStatus(int orderId, [FromQuery] string status)
+        {
+            var order = await _context.Orders.FindAsync(orderId);
+
+            if (order == null)
+                return NotFound();
+
+            order.Status = status;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(order);
+        }
+    }
+}
--- PharmacyAPI/Program.cs
+++ PharmacyAPI/Program.cs
@@ -1,14 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using PharmacyAPI.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
+
+//cors
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors("AllowAngular");
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
